93f768eef077f788a6d1729b5c7919b02648dcd2
robertopaulo <roberto123chagas@gmail.com>
2024-07-13 21:52:58 -0400

    Start

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello and welcomen!");
+        System.out.println("Hello and welscomen!");
 
         for (int i = 5; i >= 1; i--) {
             System.out.println("i = " + i);
