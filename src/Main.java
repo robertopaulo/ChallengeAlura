43a15059c13c0ee817151a7c0f0d3b55be418219
robertopaulo <roberto123chagas@gmail.com>
2024-07-13 21:46:15 -0400

    Start

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcomen!");
+
+        for (int i = 5; i >= 1; i--) {
+            System.out.println("i = " + i);
+        }
+    }
+}
